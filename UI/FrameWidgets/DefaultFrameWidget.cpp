0d8b3528c20b512ce075f36c9e97900a52c38ff4
nshcherbakova <npoplevina@yandex.ru>
2024-05-25 15:05:14 -0700

    git hooks test

--- UI/FrameWidgets/DefaultFrameWidget.cpp
+++ UI/FrameWidgets/DefaultFrameWidget.cpp
@@ -1,34 +1,26 @@
 #include "DefaultFrameWidget.h"
 #include <stdafx.h>
 
-
-
 namespace FirstYear::UI {
-DefaultFrameWidget::DefaultFrameWidget(QWidget& parent)
-    : QWidget(&parent) {
 
+DefaultFrameWidget::DefaultFrameWidget(QWidget &parent) : QWidget(&parent) {
 
   setContentsMargins(0, 0, 0, 0);
   setGeometry(parent.geometry());
   setAutoFillBackground(true);
   auto palette = QWidget::palette();
-  //palette.setColor(QPalette::Window, c_widget_background_color);
+  // palette.setColor(QPalette::Window, c_widget_background_color);
   setPalette(palette);
 
-
-  //spdlog::info("FiltersWidget UI created");
+  // spdlog::info("FiltersWidget UI created");
 
   setEnabled(false);
-
 }
 
 void DefaultFrameWidget::paintEvent(QPaintEvent *) {
-    QPainter painter(this);
+  QPainter painter(this);
 
-    // Draw background
-
+  // Draw background
 }
-
-
 
 } // namespace FirstYear::UI
