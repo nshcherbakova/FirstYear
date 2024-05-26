32e1857e75135626058aca50a046b06ea3d600c9
nshcherbakova <npoplevina@yandex.ru>
2024-05-26 14:18:04 -0700

    Logger to console and file

--- main.cpp
+++ main.cpp
@@ -10,17 +10,27 @@ void initLogger() {
   try {
     QString logs_path = QDir::currentPath() + c_log_str;
 
-    std::vector<spdlog::sink_ptr> sinks;
-    sinks.push_back(std::make_shared<spdlog::sinks::stdout_sink_st>());
-    sinks.push_back(std::make_shared<spdlog::sinks::rotating_file_sink_mt>(
+#ifdef Q_OS_ANDROID
+    auto console_logger = std::make_shared<spdlog::sinks::android_sink_st>();
+#else
+    auto console_logger = std::make_shared<spdlog::sinks::stdout_sink_st>();
+#endif
+    console_logger->set_level(spdlog::level::debug);
+
+    auto file_logger = std::make_shared<spdlog::sinks::rotating_file_sink_mt>(
         QDir().toNativeSeparators(logs_path).toStdString(), 1024 * 1024, 5,
-        false));
+        false);
+    file_logger->set_level(spdlog::level::err);
+
+    std::vector<spdlog::sink_ptr> sinks;
+    sinks.push_back(console_logger);
+    sinks.push_back(file_logger);
     auto combined_logger = std::make_shared<spdlog::logger>(
         c_logger_str, begin(sinks), end(sinks));
-    spdlog::register_logger(combined_logger);
+
     spdlog::set_default_logger(combined_logger);
     spdlog::info("Log file path {}", logs_path.toStdString());
-    // spdlog::set_default_logger(logger);
+
   } catch (const spdlog::spdlog_ex &ex) {
 #ifndef Q_OS_IOS
     // TODO fix iOS exception
@@ -42,6 +52,6 @@ int main(int argc, char *argv[]) {
   auto res = a.exec();
 
   spdlog::info("Application closed");
-  spdlog::drop_all();
+
   return res;
 }
